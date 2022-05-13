6d5447b53663772aa924c72b48869f61f2ad4e9f
satgo1546 <satgo1546@yeah.net>
2022-05-13 21:58:48 +0800

    添加录制视频功能
    Android的API什么时候才能稍微正常点？
    亦或者因为变得正常的弃用代价，永远不会？

--- app/src/main/java/com/jave/homework321002/MainActivity.kt
+++ app/src/main/java/com/jave/homework321002/MainActivity.kt
@@ -1,22 +1,34 @@
 package com.jave.homework321002
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.FileProvider
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+	private lateinit var listView: ListView
+	private lateinit var inputTitle: EditText
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 		title = "趣味音视频播放器 ←这什么土名字"
 
-		val listView = findViewById<ListView>(R.id.listView)
+		listView = findViewById(R.id.listView)
 		listView.adapter = VideosAdapter(this, (application as MyApplication).videos).also { it.filter.filter("") }
 		listView.setOnItemClickListener { adapterView, view, i, l ->
 			startActivity(Intent(this, PlayerActivity::class.java).apply {
@@ -35,13 +47,59 @@ class MainActivity : AppCompatActivity() {
 			})
 		}
 
-		findViewById<EditText>(R.id.input_title).addTextChangedListener(object : TextWatcher {
+		inputTitle = findViewById(R.id.input_title)
+		inputTitle.addTextChangedListener(object : TextWatcher {
 			override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
 				(listView.adapter as VideosAdapter).filter.filter(s.toString())
 			}
 
 			override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) = Unit
 			override fun afterTextChanged(s: Editable) = Unit
+		})
+
+		findViewById<Button>(R.id.button2).setOnClickListener { }
+
+		findViewById<Button>(R.id.button3).setOnClickListener {
+			ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO), 0)
+		}
+	}
+
+	private var recordingBasePath = ""
+
+	private val videoCaptureActivity = registerForActivityResult(StartActivityForResult()) {
+		if (it.resultCode != RESULT_OK) return@registerForActivityResult
+		File("$recordingBasePath.xml").writeText("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<i>\n</i>\n")
+		(application as MyApplication).videos.add(
+			PredefinedVideo(
+				114514,
+				SimpleDateFormat("yyyy-MM-dd", Locale.US).format(Date()),
+				"最新力作",
+				FileProvider.getUriForFile(
+					this, "$packageName.fileprovider", File("$recordingBasePath.mp4")
+				).toString(),
+				FileProvider.getUriForFile(
+					this, "$packageName.fileprovider", File("$recordingBasePath.xml")
+				).toString(),
+			)
+		)
+		(listView.adapter as VideosAdapter).filter.filter(inputTitle.text)
+	}
+
+	override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+		for (grantResult in grantResults) {
+			if (grantResult != PackageManager.PERMISSION_GRANTED) {
+				Toast.makeText(this, "权限获取失败了……", Toast.LENGTH_SHORT).show()
+				return
+			}
+		}
+		recordingBasePath = File(filesDir, SimpleDateFormat("yyyy-MM-dd", Locale.US).format(Date())).absolutePath
+		videoCaptureActivity.launch(Intent(MediaStore.ACTION_VIDEO_CAPTURE).apply {
+			putExtra(
+				MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(
+					this@MainActivity, "$packageName.fileprovider", File("$recordingBasePath.mp4")
+				)
+			)
 		})
 	}
 }
